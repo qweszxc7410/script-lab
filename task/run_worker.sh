f843f0d1508eca4f60d358406cdc42c37cac7d43
Your Name <you@example.com>
2025-07-28 22:45:31 +0800

    新增 Prefect 部署流程，包含觸發容器腳本的任務與流程定義，並優化錯誤處理

--- task/run_worker.sh
+++ task/run_worker.sh
@@ -19,3 +19,33 @@ prefect deployment build flow_run_container.py:main \
   --pool pi-workers \
   --work-queue default \
   --apply
+
+
+prefect deployment build flow_trigger.py:main \
+  --name run-inside-container \
+  --pool my-process-pool \
+  --work-queue default \
+  --apply
+
+
+from prefect import flow, task
+import subprocess
+
+@task(log_prints=True)
+def call_container_script():
+    result = subprocess.run(
+        ["bash", "/mnt/share/my_container/run.sh"],  # Host 看到的掛載路徑
+        capture_output=True,
+        text=True
+    )
+    print("STDOUT:", result.stdout)
+    print("STDERR:", result.stderr)
+    if result.returncode != 0:
+        raise RuntimeError("Script failed!")
+
+@flow(name="trigger_container_script")
+def main():
+    call_container_script()
+
+if __name__ == "__main__":
+    main()
