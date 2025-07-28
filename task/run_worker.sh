9ea24bd50d65ee8e4dca65b4e2690ba10e5bc87d
Your Name <you@example.com>
2025-07-28 22:42:44 +0800

    新增 cron.py 腳本以執行 funddj_news 流程，包含錯誤處理與執行結果回饋

--- task/run_worker.sh
+++ task/run_worker.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-prefect worker start -p default
+sudo apt update
+sudo apt install -y python3-venv python3-pip
+
+
+python3 -m venv ~/.venv-prefect
+source ~/.venv-prefect/bin/activate
+
+pip install "prefect==2.10.13" "pydantic<2.0" "anyio==3.6.2" "griffe==0.36.4"
+
+
+PREFECT_API_URL=http://192.168.1.135:4200/api prefect worker start --pool pi-workers --work-queue default
+
+
+
+
+prefect deployment build flow_run_container.py:main \
+  --name run-writer \
+  --pool pi-workers \
+  --work-queue default \
+  --apply
